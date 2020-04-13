d9775d90b7a5bb9357a4fa1abfdcbe41fdb43b25
erenhncr <erenhncr@gmail.com>
2020-04-13 12:56:47 +0300

    webpack config added

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import styles from './index.css';
+
+import numeral from 'numeral';
+
+const courseValue = numeral(1000).format('$0,0.00');
+debugger;
+console.log(`I would pay ${courseValue} for this awesome course!`);
