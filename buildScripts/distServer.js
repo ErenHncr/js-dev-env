00d8d8b33ea1dfbde2d24f2dc8e211e3e00fd705
erenhncr <erenhncr@gmail.com>
2020-04-21 02:41:49 +0300

    deploy added

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -15,14 +15,15 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.get('/users', (req, res) => {
-  // Hard coding for simplicity, pretend this hits a real database
-  res.json([
-    { "id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com" },
-    { "id": 2, "firstName": "Tammy", "lastName": "Norton", "email": "tammy@gmail.com" },
-    { "id": 3, "firstName": "Tina", "lastName": "Lee", "email": "tina@gmail.com" }
-  ]);
-});
+// It will get from heroku api
+// app.get('/users', (req, res) => {
+//   // Hard coding for simplicity, pretend this hits a real database
+//   res.json([
+//     { "id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com" },
+//     { "id": 2, "firstName": "Tammy", "lastName": "Norton", "email": "tammy@gmail.com" },
+//     { "id": 3, "firstName": "Tina", "lastName": "Lee", "email": "tina@gmail.com" }
+//   ]);
+// });
 
 app.listen(port, (err) => {
   if (err) {
--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -4,7 +4,7 @@ export default function getBaseUrl() {
   // const inDevelopment = window.location.hostname === 'localhost';
   // return inDevelopment ? 'http://localhost:3001/' : '/';
   return getQueryStringParameterByName('useMockApi') ?
-    'http://localhost:3001/' : '/';
+    'http://localhost:3001/' : 'https://safe-plains-33637.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
